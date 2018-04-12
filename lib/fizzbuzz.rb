8af8528ca8222a7ae289e9f52515429e47a667ce
William Powell <william.powell92@gmail.com>
2018-04-12 15:07:23 +0100

    Passing test for fizzbuzz(30)

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,5 @@
 def fizzbuzz(number)
-  return 'fizzbuzz' if number == 15
+  return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
   return 'buzz' if number % 5 == 0
   return 'fizz' if number % 3 == 0
   number
